105715497a2fba6d3342b467b7508153dd4c43ce
Elena Zht <elenazht@gmail.com>
2025-04-27 20:33:14 +0300

    add user request form

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [msg, setMsg] = useState('default')
+  const nameRef = useRef()
+
+  const fetchData = async(name) => {
+    try {
+      const res = await fetch(`https://deploy-server-nuoe.onrender.com/api/${name}`)
+      const data = await res.json()
+      if (data){
+        console.log(data)
+        return data
+      }
+    } catch (error) {
+      console.log(error)
+    }
+     
+  }
+
+  const handleSubmit = async() => {
+    const userName = nameRef.current.value
+    if (userName){
+      const data = await fetchData(userName)
+      if (data){
+        setMsg(data.message)
+      }
+    }
+  }
 
   return (
     <>
       <h1>welcome!</h1>
-      <button onClick={() => setCount(count+1)}>Click</button>
-      <h3>{count}</h3>
+      <input type="text" ref={nameRef}/>
+      <button onClick={handleSubmit}>say hello</button>
+      <h4>{msg}</h4>
     </>
   )
 }
